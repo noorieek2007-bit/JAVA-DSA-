43c72ed71f5cdeb4dc95984467b30cf5738beddc
Noor <noorieek200@gmail.com>
2026-03-21 11:38:58 +0530

    Added the CHAPTER 2

---
+++ CHAPTER_2/javabasics.java
@@ -0,0 +1,7 @@
+public class javabasics {
+    public static void main (String args[])
+    {
+        System.out.println("This is my first code");
+        System.out.println("Heyy I am Noor");
+    }
+}
