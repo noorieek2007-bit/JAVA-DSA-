40c3ab70ebcc82dea620e55c00ef8ee6bd5a25cd
Noor <noorieek200@gmail.com>
2026-03-24 18:57:51 +0530

    Variables basic

---
+++ CHAPTER_2/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a =6;
+        int b =8;
+        System.out.println(a);
+        System.out.println(b);
+        String c = "Noor";
+        System.out.println(c);
+    }
+}
