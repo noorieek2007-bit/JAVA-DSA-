8a90c15d9e23e2a202e797c3961de3b0146259c2
Noor <noorieek200@gmail.com>
2026-03-28 11:07:53 +0530

    datatypes

---
+++ CHAPTER_2/datatypes.java
@@ -0,0 +1,10 @@
+public class datatypes {
+    public static void main(String[] args) {
+        byte b=8; 
+        
+        System.out.println(b);
+        int a=10;
+        char d='n';
+        System.out.println(d);
+    }
+}
